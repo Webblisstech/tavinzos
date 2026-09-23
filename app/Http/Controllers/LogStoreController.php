b9e0a702b8a263b927e4a8b2373627984b34d0ee
Yekeen Oluwaseun <webblisstech@gmail.com>
2026-09-23 11:13:18 +0100

    Your commit message

--- app/Http/Controllers/LogStoreController.php
+++ app/Http/Controllers/LogStoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Exceptions\InsufficientFunds;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
